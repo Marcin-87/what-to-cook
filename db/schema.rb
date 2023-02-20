b4fed75ced84c7e1dcaae824098ec5b0fdaadda5
Toccio <luca.t.visaggio@gmail.com>
2023-02-20 16:40:52 +0100

    simple version done

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_02_18_121450) do
+ActiveRecord::Schema.define(version: 2023_02_20_152610) do
 
   create_table "recipes", force: :cascade do |t|
     t.string "name"
-    t.string "type"
+    t.string "cuisine"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
