beadfd5d83179c8ef8832f613c1809a4afb09c8c
Toccio <luca.t.visaggio@gmail.com>
2023-02-21 16:08:14 +0100

    devise added and also user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_02_20_172242) do
+ActiveRecord::Schema.define(version: 2023_02_21_144547) do
 
   create_table "recipes", force: :cascade do |t|
     t.string "name"
@@ -27,6 +27,8 @@ ActiveRecord::Schema.define(version: 2023_02_20_172242) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "first_name"
+    t.string "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
