27add8841631a675916e744e87a275e1cd1e0f9b
Marcin Niemiec <marcinniemiec87@gmail.com>
2023-02-22 17:10:43 +0100

    Ingredient model and relations between users and recipes has been created

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :user_ingredients, dependent: :destroy
+  has_many :ingredients, through: :user_ingredients
 end
